097b6000889cf3e44d424aa532ee1b808d844296
ezzyouy <82385007+ezzyouy@users.noreply.github.com>
2024-12-10 12:32:24 +0100

    v76

--- backend/routers/UserRouter.js
+++ backend/routers/UserRouter.js
@@ -1,15 +1,39 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
 import User from "../models/userModel.js";
+
 import data from "../data.js";
+import { generateToken } from "../utils.js";
 
-const userRouter= express.Router();
+const userRouter = express.Router();
 
-userRouter.get('/seed', expressAsyncHandler(async(req,res)=>{
+userRouter.get('/seed', expressAsyncHandler(async (req, res) => {
     await User.deleteMany({});
-    const createUser= await User.insertMany(data.users);
+    const createUser = await User.insertMany(data.users);
 
-    res.send({createUser});
+    res.send({ createUser });
+}));
+
+userRouter.post('/signin', expressAsyncHandler(async(req, res) => {
+    console.log(req.body);
+    
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+            res.send({
+                _id: user._id ,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user)
+            });
+            return;
+        }
+    }else{
+        res.status(404).send({message:'Invalid email or password'});
+    }
 }));
 
 export default userRouter;
--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,8 @@ mongoose.connect(process.env.MONGO_DB_URI)
     console.log(error.message);
     
 });
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.get("/api/products", (req, res) => {
     res.send(data.products)
@@ -30,7 +32,8 @@ app.get("/api/products/:id", (req, res) => {
         res.status(404).send({ message: 'Product Not Found' });
     }
 })
-app.use('/api/users', userRouter)
+app.use('/api/users', userRouter);
+
 app.get("/", (req, res) => {
     res.send("Server is ready")
 })
