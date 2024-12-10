6ea74a9940d8cc62a2ebff479373146455f7810d
ezzyouy <82385007+ezzyouy@users.noreply.github.com>
2024-12-10 01:09:14 +0100

    v75

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,42 @@
 import express from "express";
 import data from './data.js'
+import dotenv from 'dotenv'
+import mongoose from "mongoose";
+import userRouter from "./routers/UserRouter.js";
 
+dotenv.config()
 const app = express();
+
+
+mongoose.connect(process.env.MONGO_DB_URI)
+.then(()=>{
+    console.log(" mongoDb is connected");
+    
+}).catch((error)=>{
+    console.log(error.message);
+    
+});
 
 app.get("/api/products", (req, res) => {
     res.send(data.products)
 })
 app.get("/api/products/:id", (req, res) => {
-  
+
     const product = data.products.find((x) => x._id === req.params.id);
-    
-    if(product){
+
+    if (product) {
         res.send(product);
-    }else{
-        res.status(404).send({message:'Product Not Found'});
+    } else {
+        res.status(404).send({ message: 'Product Not Found' });
     }
 })
+app.use('/api/users', userRouter)
 app.get("/", (req, res) => {
     res.send("Server is ready")
+})
+
+app.use((err, req,res,next)=>{
+    res.status(500).send({message:err.message});
 })
 const port = process.env.PORT || 5002
 app.listen(port, () => {
