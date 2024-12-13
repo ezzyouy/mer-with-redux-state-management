ab486a50f5b622d8751b08ddb12ae18b356ab1e9
ezzyouy <82385007+ezzyouy@users.noreply.github.com>
2024-12-13 15:18:18 +0100

    v92

--- frontend/src/component/AdminRoute.js
+++ frontend/src/component/AdminRoute.js
@@ -2,10 +2,10 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
-function PrivateRoute({ children }) {
+function AdminRoute({ children }) {
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
   return userInfo && userInfo.isAdmin ? children : <Navigate to={"/signin"} />;
 }
 
-export default PrivateRoute;
+export default AdminRoute;
