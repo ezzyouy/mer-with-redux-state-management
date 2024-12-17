1965a0d0dfda908ddbdd1d2f0ccb449a3e787dee
ezzyouy <82385007+ezzyouy@users.noreply.github.com>
2024-12-17 13:51:24 +0100

    v107

--- frontend/src/component/Product.js
+++ frontend/src/component/Product.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import Rating from './Rating';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Rating from "./Rating";
+import { Link } from "react-router-dom";
 
 function Product(props) {
-    const {product}=props;
-    return (
-        <div className="card" key={product._id}>
-            <Link to={`/product/${product._id}`}>
-                <img className="medium" src={product.image} alt="product" />
+  const { product } = props;
+  return (
+    <div className="card" key={product._id}>
+      <Link to={`/product/${product._id}`}>
+        <img className="medium" src={product.image} alt="product" />
+      </Link>
+      <div className="card-body">
+        <Link to={`/product/${product._id}`}>
+          <h2>{product.name}</h2>
+        </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
+        <div className="row">
+          <div className="price">${product.price}</div>
+          <div>
+            <Link to={`/seller/${product?.seller?._id}`}>
+              {product.seller?.seller?.name}
             </Link>
-            <div className="card-body">
-                <Link to={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                </Link>
-               <Rating rating={product.rating} numReviews={product.numReviews}/>
-                <div className="price">${product.price}</div>
-            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
